0e16dd111eef4f57faea5f4ea53fa725f42faf07
Loi Vo Tan <dev.loivo2k2@gmail.com>
2024-02-15 00:30:36 +0700

    a

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,68 +1,71 @@
-import React, { useEffect } from 'react';
-import Sidebar from './Sidebar';
-import { Layout, theme, Button } from 'antd';
+import React, { useEffect } from "react";
+import Sidebar from "./Sidebar";
+import { Layout, theme, Button } from "antd";
 const { Content } = Layout;
-import { IntlProvider } from '@edx/frontend-platform/i18n';
-import { StudioHeader as Header } from '@edx/frontend-component-header';
-import Footer from '@edx/frontend-component-footer';
-import { Outlet } from 'react-router-dom';
-import messages from '../../i18n/index';
-import { useSelector } from 'react-redux';
+import { IntlProvider } from "@edx/frontend-platform/i18n";
+import { StudioHeader as Header } from "@edx/frontend-component-header";
+import Footer from "@edx/frontend-component-footer";
+import { Outlet } from "react-router-dom";
+import messages from "../../i18n/index";
+import { useSelector } from "react-redux";
 import {
-	fetchAuthenticatedUser,
-	getAuthenticatedHttpClient,
-	getAuthenticatedUser,
-} from '@edx/frontend-platform/auth';
+  fetchAuthenticatedUser,
+  getAuthenticatedHttpClient,
+  getAuthenticatedUser,
+  getAuthService,
+} from "@edx/frontend-platform/auth";
 
 export default function MainLayout() {
-	const {
-		token: { colorBgContainer, borderRadiusLG },
-	} = theme.useToken();
-	const auth = useSelector((state) => state.auth);
-	console.log('🚀 ~ MainLayout ~ auth:', auth);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  const auth = useSelector((state) => state.auth);
+  console.log("🚀 ~ MainLayout ~ auth:", auth);
+  const authService = getAuthService();
+  console.log("🚀 ~ MainLayout ~ authService:", authService);
 
-	return (
-		<IntlProvider locale="en" messages={messages[3]['uk']}>
-			<Layout>
-				<Header
-					title="Fast AI Studio"
-					mainMenuDropdowns={[
-						{
-							id: '1',
-							buttonTitle: 'Courses',
-							items: [
-								{
-									href: '/courses',
-									title: 'Courses',
-								},
-							],
-						},
-					]}
-					buttonTitle="hello"
-					isHiddenMainMenu={false}
-					outlineLink="/"
-					intl={messages[0]}
-				/>
-				<Content
-					style={{
-						margin: '24px 16px 0',
-					}}
-				>
-					<div
-						style={{
-							padding: 24,
-							minHeight: '100vh',
-							background: colorBgContainer,
-							borderRadius: borderRadiusLG,
-						}}
-					>
-						<Outlet />
-					</div>
-				</Content>
-				<Footer intl={messages[1]}>
-					Ant Design ©{new Date().getFullYear()} Created by Ant UED
-				</Footer>
-			</Layout>
-		</IntlProvider>
-	);
+  return (
+    <IntlProvider locale="en" messages={messages[3]["uk"]}>
+      <Layout>
+        <Header
+          title="Fast AI Studio"
+          mainMenuDropdowns={[
+            {
+              id: "1",
+              buttonTitle: "Courses",
+              items: [
+                {
+                  href: "/courses",
+                  title: "Courses",
+                },
+              ],
+            },
+          ]}
+          buttonTitle="hello"
+          isHiddenMainMenu={false}
+          outlineLink="/"
+          intl={messages[0]}
+        />
+        <Content
+          style={{
+            margin: "24px 16px 0",
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              minHeight: "100vh",
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Outlet />
+          </div>
+        </Content>
+        <Footer intl={messages[1]}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </IntlProvider>
+  );
 }
